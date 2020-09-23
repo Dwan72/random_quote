0616b553a8586d4651df436f51b5a858d6bccf7c
Donghwan An <dxa150530@utdallas.edu>
2020-09-22 22:48:54 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colors: ['#194D33', '#006B76', '#FCCB00', '#D0021B'],
+      bgColor: '#194D33',
+      pageQuote: ''
+    }
+    this.changeBgColor = this.changeBgColor.bind(this);
+  };
+
+  componentDidMount() {
+    axios.get('https://www.breakingbadapi.com/api/quote/random')
+    .then(response => {
+      let getQuote = response.data[0].quote;
+      console.log(getQuote);
+      this.setState(() => ({pageQuote: getQuote}));
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
+  }
+  componentDidUpdate(prevProps) {
+    if (this.props.bgColor !== prevProps.bgColor) {
+
+      axios.get('https://www.breakingbadapi.com/api/quote/random')
+      .then(response => {
+        let getQuote = response.data[0].quote;
+        console.log(getQuote);
+        this.setState(() => ({pageQuote: getQuote}));
+      })
+      .catch(error => {
+        console.log(error)
+      })
+
+    }
+  }
+
+  changeBgColor() {
+    let colorLength = this.state.colors.length;
+    let colorIndex = this.state.colors.indexOf(this.state.bgColor);
+
+    if (colorIndex >= colorLength - 1) {
+      this.setState((state) => ({bgColor: state.colors[0]}))
+    }
+    else {
+      this.setState((state) => ({bgColor: state.colors[colorIndex + 1]}))
+    }
+  }
+
+  render() {
+
+
+    return (
+      <div className = "backdrop"
+        style = {{backgroundColor: this.state.bgColor}}
+      >
+        <h1>Hello World</h1>
+        <button onClick = {this.changeBgColor}>change color!</button>
+        <p>{this.state.pageQuote}</p>
+      </div>
+    );
+  }
+
 }
 
 export default App;
